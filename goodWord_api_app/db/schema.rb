83a84de02fac75d5b961189b5c2fc07cbd940d3e
frinci <frinci@gmail.com>
2020-01-13 11:29:12 -0500

    attempting to add client folder

--- goodWord_api_app/db/schema.rb
+++ goodWord_api_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_022652) do
+ActiveRecord::Schema.define(version: 2020_01_13_162734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_01_13_022652) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img_url"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -37,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_01_13_022652) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img_url"
   end
 
 end
